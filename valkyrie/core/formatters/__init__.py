23ba3d4181ab7da17fa4a8fa654dc26e9faa2fa3
Einswilli <wilfriedgoeh@gmail.com>
2025-09-13 14:31:40 +0200

    Valkyrie.core: Add JSON Scann Result Formmater

--- valkyrie/core/formatters/__init__.py
+++ valkyrie/core/formatters/__init__.py
@@ -1,7 +1,11 @@
 from .base import ResultFormatter
 from .sarif import SARIFFormatter
+from .html import HTMLFormatter
+from .json import JSONFormatter
 
 __all__ = [
     ResultFormatter,
-    SARIFFormatter
+    SARIFFormatter,
+    HTMLFormatter,
+    JSONFormatter
 ]
